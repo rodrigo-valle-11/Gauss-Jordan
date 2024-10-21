a36199cebbb39140a05275be4bf862699d982071
rodrigo-valle-11 <156836812+rodrigo-valle-11@users.noreply.github.com>
2024-10-20 21:42:46 -0600

    Add files via upload

---
+++ GaussJordanMatrices/GaussJordanMatrices/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace GaussJordanMatrices
+{
+
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(330, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 308);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 403);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(506, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "INGRESE EL TAMAÑO DE LA MATRIZ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "LIMPIAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GaussJordanMatrices/GaussJordanMatrices/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GaussJordanMatrices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Llenar el ComboBox con opciones de tamaño de matriz
+            comboBox1.Items.Add("1x1");
+            comboBox1.Items.Add("2x2");
+            comboBox1.Items.Add("3x3");
+            comboBox1.Items.Add("4x4");
+
+            // Seleccionar por defecto la opción 1x1
+            comboBox1.SelectedIndex = 0;
+
+            // Generar la matriz inicial (1x1)
+            SetMatrixSize(1);
+        }
+        private void GaussJordan(double[,] matrix, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                // Hacer el elemento principal igual a 1
+                double divisor = matrix[i, i];
+                for (int j = 0; j < size + 1; j++)
+                {
+                    matrix[i, j] /= divisor;
+                }
+
+                // Hacer ceros los demás elementos de la columna
+                for (int k = 0; k < size; k++)
+                {
+                    if (k != i)
+                    {
+                        double factor = matrix[k, i];
+                        for (int j = 0; j < size + 1; j++)
+                        {
+                            matrix[k, j] -= factor * matrix[i, j];
+                        }
+                    }
+                }
+            }
+
+            // Mostrar el resultado
+            string result = "Solución:\n";
+            for (int i = 0; i < size; i++)
+            {
+                result += $"x{i + 1} = {matrix[i, size]}\n";
+            }
+            MessageBox.Show(result);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtener el tamaño de la matriz
+            int size = comboBox1.SelectedIndex + 1;
+
+            // Crear una matriz de tamaño [size, size+1] para almacenar los coeficientes
+            double[,] matrix = new double[size, size + 1];
+
+            // Llenar la matriz con los valores del DataGridView
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size + 1; j++)
+                {
+                    matrix[i, j] = Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value);
+                }
+
+                button1.FlatStyle = FlatStyle.Flat;
+                
+
+            }
+
+            // Aplicar el método de Gauss-Jordan
+            GaussJordan(matrix, size);
+
+            // Mostrar los resultados en un TextBox o donde prefieras
+            string result = "";
+            for (int i = 0; i < size; i++)
+            {
+                result += $"x{i + 1} = {matrix[i, size]}\r\n";  // Mostrar las soluciones
+            }
+
+            textBox1.Text = result;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Obtener el tamaño de la matriz seleccionado (1x1, 2x2, etc.)
+            int size = comboBox1.SelectedIndex + 1;
+
+            // Ajustar el DataGridView según el tamaño seleccionado
+            SetMatrixSize(size);
+        }
+        private void SetMatrixSize(int size)
+        {
+            // Configurar el número de filas y columnas
+            dataGridView1.RowCount = size;
+            dataGridView1.ColumnCount = size + 1; // Columna adicional para el resultado
+
+            // Ajustar los encabezados de las columnas (opcionales)
+            for (int i = 0; i < size; i++)
+            {
+                dataGridView1.Columns[i].HeaderText = "x" + (i + 1);  // Encabezados de las variables
+            }
+            dataGridView1.Columns[size].HeaderText = "Resultado";  // Encabezado para el término independiente
+        }
+
+        // Método para resolver la matriz usando el método de Gauss-Jordan
+        private void SolveGaussJordan(double[,] matrix, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                // Asegurarse de que el pivote no sea 0, si lo es intercambiar filas
+                if (matrix[i, i] == 0)
+                {
+                    bool swapped = false;
+                    for (int k = i + 1; k < size; k++)
+                    {
+                        if (matrix[k, i] != 0)
+                        {
+                            SwapRows(matrix, i, k, size);
+                            swapped = true;
+                            break;
+                        }
+                    }
+                    if (!swapped)
+                    {
+                        MessageBox.Show("No se puede resolver el sistema: hay una fila de ceros.");
+                        return;
+                    }
+                }
+
+                // Hacer que el pivote sea 1
+                double pivot = matrix[i, i];
+                for (int j = 0; j < size + 1; j++)
+                {
+                    matrix[i, j] /= pivot;
+                }
+
+                // Hacer ceros los elementos de la columna actual, excepto el pivote
+                for (int k = 0; k < size; k++)
+                {
+                    if (k != i)
+                    {
+                        double factor = matrix[k, i];
+                        for (int j = 0; j < size + 1; j++)
+                        {
+                            matrix[k, j] -= factor * matrix[i, j];
+                        }
+                    }
+                }
+            }
+        }
+        // Método para intercambiar dos filas de la matriz
+        private void SwapRows(double[,] matrix, int row1, int row2, int size)
+        {
+            for (int i = 0; i < size + 1; i++)
+            {
+                double temp = matrix[row1, i];
+                matrix[row1, i] = matrix[row2, i];
+                matrix[row2, i] = temp;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Ajustar el estilo del DataGridView
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+
+            // Ajustar el tamaño del formulario y el TextBox
+            this.Font = new Font("Segoe UI", 10);
+            textBox1.Multiline = true;  // Para que se vea mejor el TextBox
+            textBox1.Height = 50;
+
+            // Estilo de los botones
+            button1.FlatStyle = FlatStyle.Flat;
+            button2.FlatStyle = FlatStyle.Flat;
+
+            // Otros ajustes que ya tenías
+            comboBox1.Items.Add("1x1");
+            comboBox1.Items.Add("2x2");
+            comboBox1.Items.Add("3x3");
+            comboBox1.Items.Add("4x4");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Limpiar el contenido del DataGridView
+            dataGridView1.Rows.Clear();
+
+            // foreach (DataGridViewRow row in dataGridView1.Rows)
+            // {
+            //     foreach (DataGridViewCell cell in row.Cells)
+            //     {
+            //         cell.Value = null; // Borra los valores de cada celda
+            //     }
+            // }
+
+            // Limpiar el contenido del TextBox de resultados
+            textBox1.Clear();
+
+            // Reiniciar la selección en el ComboBox a 1x1 (opcional)
+            comboBox1.SelectedIndex = 0;
+
+            // Ajustar el tamaño de la matriz a 1x1 por defecto (opcional)
+            SetMatrixSize(1);
+        }
+    }
+}
+        
+
+    
+
